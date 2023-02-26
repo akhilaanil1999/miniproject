02f81e7e99249662c4948ed1d6b4fa7c831457ec
akhilaanil1999 <akhila.anil.911@gmail.com>
2023-02-26 21:52:56 +0530

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+  path:'', component:HomeComponent
+  },
+  {
+
+  path:'home', component:HomeComponent
+  },
+
+  {
+    path:'aboutus', component:AboutusComponent
+  },
+  {
+    path:'contact', component:ContactComponent
+  },
+  {
+    path:'feedback',component:FeedbackComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { FooterComponent } from './utils/footer/footer.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutusComponent,
+    ContactComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
